5e872a22a91a2b83035b9f98335ce715bc826939
gokhanunal <gokhanunal.mdbf20@iste.edu.tr>
2023-05-29 20:40:34 +0300

    Hatalar düzeltildi

---
+++ YMG22-23/YurtKayitSistemi/FrmOgr.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace YurtKayitSistemi
+{
+    partial class FrmOgr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.RchAdres = new System.Windows.Forms.RichTextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.MskVeliTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.TxtVeliAdSoyad = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.CmbOdaNo = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CmbBolum = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MskDogum = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MskOgrTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtOgrAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(294, 549);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 30);
+            this.button1.TabIndex = 45;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // RchAdres
+            // 
+            this.RchAdres.Location = new System.Drawing.Point(222, 373);
+            this.RchAdres.Name = "RchAdres";
+            this.RchAdres.Size = new System.Drawing.Size(282, 170);
+            this.RchAdres.TabIndex = 44;
+            this.RchAdres.Text = "";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(170, 373);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 20);
+            this.label11.TabIndex = 43;
+            this.label11.Text = "Adres:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(129, 340);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(96, 20);
+            this.label10.TabIndex = 42;
+            this.label10.Text = "Veli Telefon:";
+            // 
+            // MskVeliTelefon
+            // 
+            this.MskVeliTelefon.BackColor = System.Drawing.Color.GreenYellow;
+            this.MskVeliTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskVeliTelefon.Location = new System.Drawing.Point(231, 336);
+            this.MskVeliTelefon.Mask = "(999) 000-0000";
+            this.MskVeliTelefon.Name = "MskVeliTelefon";
+            this.MskVeliTelefon.Size = new System.Drawing.Size(273, 24);
+            this.MskVeliTelefon.TabIndex = 41;
+            // 
+            // TxtVeliAdSoyad
+            // 
+            this.TxtVeliAdSoyad.BackColor = System.Drawing.Color.GreenYellow;
+            this.TxtVeliAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtVeliAdSoyad.Location = new System.Drawing.Point(231, 304);
+            this.TxtVeliAdSoyad.Name = "TxtVeliAdSoyad";
+            this.TxtVeliAdSoyad.Size = new System.Drawing.Size(273, 26);
+            this.TxtVeliAdSoyad.TabIndex = 40;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(113, 307);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(112, 20);
+            this.label9.TabIndex = 39;
+            this.label9.Text = "Veli Ad Soyad:";
+            // 
+            // CmbOdaNo
+            // 
+            this.CmbOdaNo.BackColor = System.Drawing.Color.GreenYellow;
+            this.CmbOdaNo.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmbOdaNo.FormattingEnabled = true;
+            this.CmbOdaNo.Location = new System.Drawing.Point(231, 269);
+            this.CmbOdaNo.Name = "CmbOdaNo";
+            this.CmbOdaNo.Size = new System.Drawing.Size(273, 29);
+            this.CmbOdaNo.TabIndex = 38;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(158, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 20);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Oda No:";
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.BackColor = System.Drawing.Color.GreenYellow;
+            this.TxtMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtMail.Location = new System.Drawing.Point(231, 237);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(273, 26);
+            this.TxtMail.TabIndex = 36;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(184, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 20);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Mail:";
+            // 
+            // CmbBolum
+            // 
+            this.CmbBolum.BackColor = System.Drawing.Color.GreenYellow;
+            this.CmbBolum.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmbBolum.FormattingEnabled = true;
+            this.CmbBolum.Location = new System.Drawing.Point(231, 197);
+            this.CmbBolum.Name = "CmbBolum";
+            this.CmbBolum.Size = new System.Drawing.Size(273, 29);
+            this.CmbBolum.TabIndex = 34;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(108, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 20);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Öğrenci Bölüm:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(118, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Doğum Tarihi:";
+            // 
+            // MskDogum
+            // 
+            this.MskDogum.BackColor = System.Drawing.Color.GreenYellow;
+            this.MskDogum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskDogum.Location = new System.Drawing.Point(231, 166);
+            this.MskDogum.Mask = "00/00/0000";
+            this.MskDogum.Name = "MskDogum";
+            this.MskDogum.Size = new System.Drawing.Size(273, 24);
+            this.MskDogum.TabIndex = 31;
+            this.MskDogum.ValidatingType = typeof(System.DateTime);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(159, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Telefon:";
+            // 
+            // MskOgrTelefon
+            // 
+            this.MskOgrTelefon.BackColor = System.Drawing.Color.GreenYellow;
+            this.MskOgrTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskOgrTelefon.Location = new System.Drawing.Point(231, 136);
+            this.MskOgrTelefon.Mask = "(999) 000-0000";
+            this.MskOgrTelefon.Name = "MskOgrTelefon";
+            this.MskOgrTelefon.Size = new System.Drawing.Size(273, 24);
+            this.MskOgrTelefon.TabIndex = 29;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(192, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "TC:";
+            // 
+            // MskTC
+            // 
+            this.MskTC.BackColor = System.Drawing.Color.GreenYellow;
+            this.MskTC.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskTC.Location = new System.Drawing.Point(231, 106);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(273, 24);
+            this.MskTC.TabIndex = 27;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.GreenYellow;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(231, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 26);
+            this.textBox1.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(108, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Öğrenci Soyad:";
+            // 
+            // TxtOgrAd
+            // 
+            this.TxtOgrAd.BackColor = System.Drawing.Color.GreenYellow;
+            this.TxtOgrAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrAd.Location = new System.Drawing.Point(231, 42);
+            this.TxtOgrAd.Name = "TxtOgrAd";
+            this.TxtOgrAd.Size = new System.Drawing.Size(273, 26);
+            this.TxtOgrAd.TabIndex = 24;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(133, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Öğrenci Ad:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 621);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RchAdres);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.MskVeliTelefon);
+            this.Controls.Add(this.TxtVeliAdSoyad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.CmbOdaNo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.CmbBolum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.MskDogum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MskOgrTelefon);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtOgrAd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox RchAdres;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.MaskedTextBox MskVeliTelefon;
+        private System.Windows.Forms.TextBox TxtVeliAdSoyad;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox CmbOdaNo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox CmbBolum;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox MskDogum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox MskOgrTelefon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtOgrAd;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ YMG22-23/YurtKayitSistemi/FrmOgr.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace YurtKayitSistemi
+{
+    public partial class FrmOgr : Form
+    {
+        public FrmOgr()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Data Source=DESKTOP-LJ30V5U\SQLEXPRESS;Initial Catalog=YurtKayit;Integrated Security=True
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-LJ30V5U\\SQLEXPRESS;Initial Catalog=YurtKayit;Integrated Security=True");
+            public FrmOgrKayit()
+            {
+
+            }
+
+            private void TxtOgrAd_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void FrmOgr_Load(object sender, EventArgs e)
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("Select BolumAd From Bolumler", baglanti);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    CmbBolum.Items.Add(oku[0].ToString());
+                }
+                baglanti.Close();
+
+                //Bos Odalari Listeleme Komutlari
+                baglanti.Open();
+                SqlCommand komut2 = new SqlCommand("Select OdaNo From Odalar where OdaKapasite != OdaAktif", baglanti);
+                SqlDataReader oku2 = komut2.ExecuteReader();
+                while (oku2.Read())
+                {
+                    CmbOdaNo.Items.Add(oku2[0].ToString());
+                }
+                baglanti.Close();
+                // dolu odalari l'steleme
+            }
+        }
+    }
+}
