9eca41cb988154567246198668948c5267d66267
gokhanunal <gokhanunal.mdbf20@iste.edu.tr>
2023-06-02 23:45:09 +0300

    Aciklama yap

---
+++ YMG22-23/YurtOt/YurtOt/FormOgrKayit.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace YurtOt
+{
+    partial class FormOgrKayit
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtOgrAd = new System.Windows.Forms.TextBox();
+            this.TxtOgrSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MskTC = new System.Windows.Forms.MaskedTextBox();
+            this.MskOgrTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MskDogum = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CmbBolum = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CmbOdaNo = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TxtVeliAdSoyad = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.MskVeliTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.RchAdres = new System.Windows.Forms.RichTextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(47, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğrenci Ad:";
+            // 
+            // TxtOgrAd
+            // 
+            this.TxtOgrAd.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrAd.Location = new System.Drawing.Point(171, 31);
+            this.TxtOgrAd.Name = "TxtOgrAd";
+            this.TxtOgrAd.Size = new System.Drawing.Size(309, 23);
+            this.TxtOgrAd.TabIndex = 1;
+            // 
+            // TxtOgrSoyad
+            // 
+            this.TxtOgrSoyad.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtOgrSoyad.Location = new System.Drawing.Point(171, 63);
+            this.TxtOgrSoyad.Name = "TxtOgrSoyad";
+            this.TxtOgrSoyad.Size = new System.Drawing.Size(309, 23);
+            this.TxtOgrSoyad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(17, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Öğrenci Soyad:";
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtMail.Location = new System.Drawing.Point(171, 234);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(309, 23);
+            this.TxtMail.TabIndex = 5;
+            this.TxtMail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(127, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TC:";
+            // 
+            // MskTC
+            // 
+            this.MskTC.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskTC.Location = new System.Drawing.Point(171, 94);
+            this.MskTC.Mask = "00000000000";
+            this.MskTC.Name = "MskTC";
+            this.MskTC.Size = new System.Drawing.Size(309, 23);
+            this.MskTC.TabIndex = 6;
+            this.MskTC.ValidatingType = typeof(int);
+            // 
+            // MskOgrTelefon
+            // 
+            this.MskOgrTelefon.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskOgrTelefon.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.MskOgrTelefon.Location = new System.Drawing.Point(171, 123);
+            this.MskOgrTelefon.Mask = "(999) 000-0000";
+            this.MskOgrTelefon.Name = "MskOgrTelefon";
+            this.MskOgrTelefon.Size = new System.Drawing.Size(309, 23);
+            this.MskOgrTelefon.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(77, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefon:";
+            // 
+            // MskDogum
+            // 
+            this.MskDogum.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskDogum.Location = new System.Drawing.Point(171, 160);
+            this.MskDogum.Mask = "00/00/0000";
+            this.MskDogum.Name = "MskDogum";
+            this.MskDogum.Size = new System.Drawing.Size(309, 23);
+            this.MskDogum.TabIndex = 10;
+            this.MskDogum.ValidatingType = typeof(System.DateTime);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(27, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Doğum Tarihi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(17, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Öğrenci Bölüm:";
+            // 
+            // CmbBolum
+            // 
+            this.CmbBolum.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CmbBolum.FormattingEnabled = true;
+            this.CmbBolum.Location = new System.Drawing.Point(171, 198);
+            this.CmbBolum.Name = "CmbBolum";
+            this.CmbBolum.Size = new System.Drawing.Size(308, 30);
+            this.CmbBolum.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(107, 241);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Mail:";
+            // 
+            // CmbOdaNo
+            // 
+            this.CmbOdaNo.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CmbOdaNo.FormattingEnabled = true;
+            this.CmbOdaNo.Location = new System.Drawing.Point(171, 263);
+            this.CmbOdaNo.Name = "CmbOdaNo";
+            this.CmbOdaNo.Size = new System.Drawing.Size(309, 30);
+            this.CmbOdaNo.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(87, 270);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Oda No:";
+            // 
+            // TxtVeliAdSoyad
+            // 
+            this.TxtVeliAdSoyad.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtVeliAdSoyad.Location = new System.Drawing.Point(171, 299);
+            this.TxtVeliAdSoyad.Name = "TxtVeliAdSoyad";
+            this.TxtVeliAdSoyad.Size = new System.Drawing.Size(309, 23);
+            this.TxtVeliAdSoyad.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(17, 302);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(148, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Veli Ad Soyad:";
+            // 
+            // MskVeliTelefon
+            // 
+            this.MskVeliTelefon.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskVeliTelefon.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.MskVeliTelefon.Location = new System.Drawing.Point(170, 328);
+            this.MskVeliTelefon.Mask = "(999) 000-0000";
+            this.MskVeliTelefon.Name = "MskVeliTelefon";
+            this.MskVeliTelefon.Size = new System.Drawing.Size(309, 23);
+            this.MskVeliTelefon.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(27, 331);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(138, 16);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Veli Telefon:";
+            // 
+            // RchAdres
+            // 
+            this.RchAdres.Location = new System.Drawing.Point(172, 367);
+            this.RchAdres.Name = "RchAdres";
+            this.RchAdres.Size = new System.Drawing.Size(308, 237);
+            this.RchAdres.TabIndex = 20;
+            this.RchAdres.Text = "";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(97, 367);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(68, 16);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Adres:";
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKaydet.Location = new System.Drawing.Point(262, 620);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(127, 42);
+            this.BtnKaydet.TabIndex = 22;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // FormOgrKayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 713);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.RchAdres);
+            this.Controls.Add(this.MskVeliTelefon);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.TxtVeliAdSoyad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.CmbOdaNo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.CmbBolum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.MskDogum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.MskOgrTelefon);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MskTC);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtOgrSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtOgrAd);
+            this.Controls.Add(this.label1);
+            this.Name = "FormOgrKayit";
+            this.Text = "Öğrenci Kayıt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtOgrAd;
+        private System.Windows.Forms.TextBox TxtOgrSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox MskTC;
+        private System.Windows.Forms.MaskedTextBox MskOgrTelefon;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox MskDogum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox CmbBolum;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox CmbOdaNo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TxtVeliAdSoyad;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.MaskedTextBox MskVeliTelefon;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox RchAdres;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button BtnKaydet;
+    }
+}
+
